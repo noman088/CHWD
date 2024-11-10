de7d1aa866556d3d6ff61f3b133216b63bf5a0e0
Noman Ahmad <mdnoman70499@gmail.com>
2024-11-10 10:33:50 +0530

    Started Js with notes

---
+++ Javascript/index.js
@@ -0,0 +1,43 @@
+// line below is used to print the console window
+
+// console.log("Namaste Duniya Version 4");
+// marks = 27;
+// if (marks >= 90) {
+//     console.log("A");
+// } else if (marks >= 80) {
+//     console.log("B");
+// } else if (marks >= 70) {
+//     console.log("C");
+// } else {
+//     console.log("D");
+// }
+// let num = "a";
+
+// switch (num) {
+//     case 1:
+//         console.log("A");
+//         break;
+//     case 2:
+//         console.log("B");
+//         break;
+//     case 3:
+//         console.log("C");
+//         break;
+//     default:
+//         console.log("Please choose number between 1 to 3");
+// }
+
+// for (let i = 1; i < 5; i++) {
+//     console.log("Noman " + i);
+// }
+// let i = 1;
+// while (i <= 5) {
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 1;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 10);
